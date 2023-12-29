3d9809057402eb90d0b3552e7ba1970ebcb6eeef
Joaquin Mendoza <joaquinmendoza707@gmail.com>
2023-12-29 14:35:45 -0800

    Updated reqs

--- run.py
+++ run.py
@@ -1,4 +1,10 @@
-from src.index import app
+import sys
 import os
+
+# Add the project root directory to the path
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+from src.index import app
+
 
 app.run(host='0.0.0.0', port=os.environ.get('PORT'), use_reloader=False)
